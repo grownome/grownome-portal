b1f70b29778c15f8b21d8b6a28864c62ad197886
Eric Fode <ericfode@gmail.com>
2018-10-13 15:29:51 -0700

    got rid of some cruft

--- resources/migrations/20181007174951-add-images-table.up.sql
+++ resources/migrations/20181007174951-add-images-table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE images(
   md5              UUID           PRIMARY KEY,
-  device_id        INTEGER        REFERENCES devices(id),
+  device_id        BIGSERIAL      REFERENCES devices(id),
   path             VARCHAR (355)  NOT NULL,
   created_on       TIMESTAMP      NOT NULL
 );
